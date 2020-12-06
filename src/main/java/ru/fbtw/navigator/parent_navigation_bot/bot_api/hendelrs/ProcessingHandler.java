3aa3fce088cb015743d9146749f38a5e14917986
netos23 <nikmoroz2002@outlook.com>
2020-12-06 20:55:48 +0300

    add processing handler

--- src/main/java/ru/fbtw/navigator/parent_navigation_bot/bot_api/hendelrs/ProcessingHandler.java
+++ src/main/java/ru/fbtw/navigator/parent_navigation_bot/bot_api/hendelrs/ProcessingHandler.java
@@ -1,4 +1,40 @@
 package ru.fbtw.navigator.parent_navigation_bot.bot_api.hendelrs;
 
-public class ProcessingHandler {
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import ru.fbtw.navigator.parent_navigation_bot.bot_api.BotState;
+import ru.fbtw.navigator.parent_navigation_bot.bot_api.concurent.ConcurrentItem;
+import ru.fbtw.navigator.parent_navigation_bot.service.ReplyMessagesService;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Component
+public class ProcessingHandler implements InputMessageHandler {
+    private final ReplyMessagesService messagesService;
+
+    public ProcessingHandler(ReplyMessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+
+    @Override
+    public BotApiMethod<?> handle(Message message) {
+        return processUserInput(message);
+    }
+
+    private SendMessage processUserInput(Message message) {
+        long chatId = message.getChatId();
+        return messagesService.getReplyMessage(chatId, "reply.searchBegin");
+    }
+
+    @Override
+    public BotState[] getHandlerName() {
+        return new BotState[]{BotState.PROCESSING};
+    }
+
+    @Override
+    public void setQueue(ConcurrentLinkedQueue<ConcurrentItem> contentQueue) {
+
+    }
 }
